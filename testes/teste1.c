176de15665459b7bf23ecf303bb85dbefec2d5ea
luabrantess <analuizaabr4@gmail.com>
2026-04-15 17:33:28 -0300

    casos de teste 1 ao 5 com comentarios

--- testes/teste1.c
+++ testes/teste1.c
@@ -4,8 +4,12 @@ int main() {
     int x = 10;
 
     if (x > 5) {
-        x = x + 1;
+        printf("Teste certo\n");
+    } else {
+        printf("Teste errado\n");
     }
 
     return 0;
 }
+
+// O teste não passou com o # e nem com a função main
--- testes/teste4.c
+++ testes/teste4.c
@@ -1,4 +1,7 @@
-x = [1, 2, 3, 4, 5]
+x = [1, 2, 3, 4, 5];
 
-for i in x:
-    print(i)
+for i in x{
+    print(i);
+}
+
+// Não reconhece listas, que são importantes para o for
